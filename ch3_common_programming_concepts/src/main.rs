f8ee0dabe69fd78c34c7ecefdb3dca8c85e6a1dc
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-11-03 22:12:59 +0530

    datatypes in rust

--- ch3_common_programming_concepts/src/main.rs
+++ ch3_common_programming_concepts/src/main.rs
@@ -38,3 +38,68 @@
 
 
 
+// Datatype 
+ 
+fn main (){
+
+    // integers 
+    // let x :u32 = 5;
+    // let y :i32 = -6;
+    // println!("the value of x is :{x}");
+    // println!("the value of y is :{y}");
+
+    // floating point 
+    // let x = 2.0; //f64 by default 
+    // let y : f32 = 5.0;
+    // println!("the value of x is :{x}");
+    // println!("the value of y is :{y}");
+
+
+    // numeric operations 
+//     fn main() {
+//     // addition
+//     let sum = 5 + 10;
+
+//     // subtraction
+//     let difference = 95.5 - 4.3;
+
+//     // multiplication
+//     let product = 4 * 30;
+
+//     // division
+//     let quotient = 56.7 / 32.2;
+//     let truncated = -5 / 3; // Results in -1
+
+//     // remainder
+//     let remainder = 43 % 5;
+// }
+
+
+// boolean 
+// let t:bool  = true ;
+// let f:bool = false ;
+// println!("The value of t is :{t}");
+// println!("The value of f is :{f}");
+
+// character type 
+// let c:char = 'z';
+
+// tuple 
+// let tup:(i32 , f64 , u8) = (500, 6.4, 5);
+
+// let (x,y,z) = tup ;
+// println!("the value of x is :{x}")
+
+// Array -> used when we know the number of elements and if we dont know that measn size can shrink or increase we use vector 
+let arr:[i32 , 5] = [1,2,3,4,5];
+let first = a[0];
+    let second = a[1];
+
+
+let a = [3;5]; // same as a = [3,3,3,3,3];
+
+
+}
+
+
+
