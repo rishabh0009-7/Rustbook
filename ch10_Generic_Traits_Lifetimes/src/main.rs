75d2bf6abcf20f3bc7e1f70c798c63bcd46efc0a
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-12-05 12:21:13 +0530

    generics

--- ch10_Generic_Traits_Lifetimes/src/main.rs
+++ ch10_Generic_Traits_Lifetimes/src/main.rs
@@ -1,25 +1,63 @@
 // generic 
 // to find largest  value 
 
-fn largest<T:std::cmp::PartialOrd>(list:&[T])->&T{
-    let mut largest = &list[0];
+// fn largest<T:std::cmp::PartialOrd>(list:&[T])->&T{
+//     let mut largest = &list[0];
 
-    for item in list {
-        if item > largest{
-            largest = item ;
-        }
+//     for item in list {
+//         if item > largest{
+//             largest = item ;
+//         }
+
+//     }
+//     largest 
+
+
+// }
+
+
+
+// fn main (){
+//     let number_list = vec![25,4,67, 90];
+
+//     let result  = largest(&number_list);
+//     println!("The largest number is :{result}");
+// }
+
+
+
+struct Point <T> {
+    x:T,
+    y:T,
+
+}
+
+
+impl<T>  Point<T> {
+    fn  x (&self)-> &T{
+        &self.x
 
     }
-    largest 
-
 
 }
 
 
 
 fn main (){
-    let number_list = vec![25,4,67, 90];
+    let integer = Point{x:5 , y:6};
+    let float = Point {x:3.5 , 7.8};
 
-    let result  = largest(&number_list);
-    println!("The largest number is :{result}");
 }
+
+
+
+// enum Option<T> {
+//     Some(T),
+//     None,
+// }
+
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
+
