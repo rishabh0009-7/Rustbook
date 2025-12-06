feb5b2bd460a55d20fcb5d5160b184b75b03da8d
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-12-06 13:34:12 +0530

    Lifetimes in rust

--- ch10_Generic_Traits_Lifetimes/src/main.rs
+++ ch10_Generic_Traits_Lifetimes/src/main.rs
@@ -64,63 +64,90 @@ fn main (){
 
 
 // Traits 
-pub trait Summary {
-    fn  summarize (&self) -> String {
+// pub trait Summary {
+//     fn  summarize (&self) -> String {
 
-    }
-}
-
-
-
-pub struct Newsletter {
-    pub Headline :String ,
-    pub location :String ,
-    pub author :String,
-    pub content :String ,
+//     }
+// }
 
 
 
-}
+// pub struct Newsletter {
+//     pub Headline :String ,
+//     pub location :String ,
+//     pub author :String,
+//     pub content :String ,
 
-impl  Summary for Newsletter{
-    fn summarize(&self) -> String {
-        format!("{} , by {}  ({})", self.Headline , self.author , self.location)
 
-    }
-}
+
+// }
+
+// impl  Summary for Newsletter{
+//     fn summarize(&self) -> String {
+//         format!("{} , by {}  ({})", self.Headline , self.author , self.location)
+
+//     }
+// }
 
 
 // Traits as Parameters
 
-pub fn notify (item :&Summary){
-    println!("Breaking news {}" , item.summarize())
+// pub fn notify (item :&Summary){
+//     println!("Breaking news {}" , item.summarize())
 
-}
+// }
 
 
 // Trait bounds  // better way to write 
-pub fn notify<T:Summary> (item :&T) {
-    println!("Breaking news {}" , item.summarize())
+// pub fn notify<T:Summary> (item :&T) {
+//     println!("Breaking news {}" , item.summarize())
 
 
 
-}
+// }
 
 
 // Clearer Trait Bounds with where Clauses
 
-fn some_function <T, U>(t:&T , u:&U) -> i32 
-where 
-T:Display+ clone , 
-U :Clone + Debug , {
+// fn some_function <T, U>(t:&T , u:&U) -> i32 
+// where 
+// T:Display+ clone , 
+// U :Clone + Debug , {
 
 
-}
+// }
 
 
 
 // Returning Types That Implement Traits
 
-fn return_summarizable () -> impl Summary {
+// fn return_summarizable () -> impl Summary {
 
+// }
+
+
+
+// Lifetimes 
+// The main aim of lifetimes is to prevent dangling references, which cause a program to reference data other than the data it’s intended to reference
+
+// fn main (){
+//     let r ;
+
+//     {
+//         let x = 6 ;
+//         r = &x ;
+
+//     }
+//     println!("the value of r is :{r}"); // will not compile r gone out of scope 
+// }
+
+
+fn main (){
+    let x = 6;
+
+    let r = &x ;
+
+
+    println!("the value of r is :{r}");
 }
+
