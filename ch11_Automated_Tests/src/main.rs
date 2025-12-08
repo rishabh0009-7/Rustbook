3faa01ce82fee98a2f443e3d453cdf6883d465e2
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-12-08 19:53:52 +0530

    Tests

--- ch11_Automated_Tests/src/main.rs
+++ ch11_Automated_Tests/src/main.rs
@@ -1,15 +1,75 @@
-pub fn add(left :u64 , right:u64)-> u64 {
-    left + right 
+// pub fn add(left :u64 , right:u64)-> u64 {
+//     left + right 
+// }
+
+// #[cfg(test)]
+
+// mod tests {
+//     use super::*;
+//     #[test]
+
+// fn it_works(){
+//     let result = add(2,2 );
+//     assert_eq!(result , 4); // test will pass 
+// }
+
+
+// #[test]
+// fn another (){
+//     panic!("make this program fail ");
+// }
+// }
+
+
+
+
+
+#[derive(Debug)]
+struct Rectangle {
+    width:u32,
+    height:u32,
+}
+
+impl Rectangle  {
+    fn  can_hold (&self , other:&Rectangle )-> bool{
+        self.width> other.width && self.height > other.height
+
+    }
 }
 
 #[cfg(test)]
-
 mod tests {
     use super::*;
-    #[test]
 
-fn it_works(){
-    let result = add(2,2 );
-    assert_eq!(result , 4);
-}
+    #[test]
+    fn larger_can_hold_smaller (){
+        let larger = Rectangle {
+            width: 8,
+            height : 7 ,
+        };
+
+        let smaller = Rectangle {
+            width : 5,
+            height :1 ,
+        };
+
+        assert!(larger.can_hold(&smaller));
+
+
+    }
+#[test]
+    fn smaller_cannot_hold_larger (){
+        let larger = Rectangle {
+            width: 8,
+            height : 7 ,
+        };
+
+        let smaller = Rectangle {
+            width : 5,
+            height :1 ,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+
+    }
 }
